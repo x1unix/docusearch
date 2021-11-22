a54738784d9a05dfdcc83262c61c113b3e9f25ec
x1unix <denis0051@gmail.com>
2021-11-22 10:19:00 +0200

    store: finish synced store tests

--- internal/services/store/store_synced_test.go
+++ internal/services/store/store_synced_test.go
@@ -52,6 +52,26 @@ func TestSyncedDocumentStore_AddDocument(t *testing.T) {
 				return sp
 			},
 		},
+		"should respect index settings": {
+			name: "correct",
+			data: strings.NewReader("The quick brown fox jumps over the lazy dog"),
+			cfg:  TextIndexConfig{IgnoreCommonWords: false},
+
+			newStoreFn: func(t *testing.T, ctrl *gomock.Controller) DocumentStore {
+				store := mocks.NewMockDocumentStore(ctrl)
+				store.EXPECT().
+					AddDocument(gomock.Any(), "correct", matchReaderContents(t, []byte("The quick brown fox jumps over the lazy dog"))).
+					Return(nil)
+				return store
+			},
+
+			newSearchFn: func(t *testing.T, ctrl *gomock.Controller) search.Provider {
+				sp := mocks.NewMockProvider(ctrl)
+				expectWords := search.WordsFromString("The quick brown fox jumps over the lazy dog", nil)
+				sp.EXPECT().AddDocumentRef(gomock.Any(), "correct", stringsContentsMatch(t, expectWords)).Return(nil)
+				return sp
+			},
+		},
 		"should raise errors from inner storage": {
 			name: "bad",
 			data: strings.NewReader("foobar"),
@@ -92,6 +112,78 @@ func TestSyncedDocumentStore_AddDocument(t *testing.T) {
 			require.NoError(t, err)
 		})
 	}
+}
+
+func TestSyncedDocumentStore_RemoveDocument(t *testing.T) {
+	cases := map[string]struct {
+		name    string
+		wantErr string
+
+		wantErrFn   func(err error) bool
+		newStoreFn  func(t *testing.T, ctrl *gomock.Controller) DocumentStore
+		newSearchFn func(t *testing.T, ctrl *gomock.Controller) search.Provider
+	}{
+		"should remove document from index": {
+			name:    "foobar",
+			wantErr: "failed to remove document from search index: test",
+			newStoreFn: func(t *testing.T, ctrl *gomock.Controller) DocumentStore {
+				store := mocks.NewMockDocumentStore(ctrl)
+				store.EXPECT().RemoveDocument(gomock.Any(), "foobar").Return(nil)
+				return store
+			},
+			newSearchFn: func(t *testing.T, ctrl *gomock.Controller) search.Provider {
+				sp := mocks.NewMockProvider(ctrl)
+				sp.EXPECT().RemoveDocumentRef(gomock.Any(), "foobar").Return(errors.New("test"))
+				return sp
+			},
+		},
+		"should stop if document wasn't removed properly": {
+			name: "foobar",
+			wantErrFn: func(err error) bool {
+				return errors.Is(err, fs.ErrNotExist)
+			},
+			newStoreFn: func(t *testing.T, ctrl *gomock.Controller) DocumentStore {
+				store := mocks.NewMockDocumentStore(ctrl)
+				store.EXPECT().RemoveDocument(gomock.Any(), "foobar").Return(fs.ErrNotExist)
+				return store
+			},
+			newSearchFn: func(t *testing.T, ctrl *gomock.Controller) search.Provider {
+				return nil
+			},
+		},
+	}
+
+	for n, c := range cases {
+		t.Run(n, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			syncedStore := NewSyncedDocumentStore(zaptest.NewLogger(t), c.newStoreFn(t, ctrl),
+				c.newSearchFn(t, ctrl), TextIndexConfig{})
+			err := syncedStore.RemoveDocument(nil, c.name)
+			if c.wantErr != "" {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), c.wantErr)
+				return
+			}
+
+			if c.wantErrFn != nil {
+				require.Error(t, err)
+				require.True(t, c.wantErrFn(err))
+				return
+			}
+
+			require.NoError(t, err)
+		})
+	}
+}
+
+func TestSyncedDocumentStore_GetDocument(t *testing.T) {
+	wantErr := "error from inner call"
+	ctrl := gomock.NewController(t)
+	storeMock := mocks.NewMockDocumentStore(ctrl)
+	storeMock.EXPECT().GetDocument("testdoc").Return(nil, errors.New(wantErr))
+	syncStore := NewSyncedDocumentStore(nil, storeMock, nil, TextIndexConfig{})
+	_, err := syncStore.GetDocument("testdoc")
+	require.EqualError(t, err, wantErr)
 }
 
 type readerMatcher struct {
